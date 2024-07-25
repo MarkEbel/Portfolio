d8b9391b338c35a2b272c14222b42edd1fe8d7a0
Mark Ebel <markbenebel@gmail.com>
2024-07-25 09:41:50 +0100

    better css styling

--- myOwnWebsite/src/components/Navbar.tsx
+++ myOwnWebsite/src/components/Navbar.tsx
@@ -4,10 +4,12 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-name">Mark Ebel</div>
-      <ThemeToggle />
-      <ul>
+    <nav className="navbar"> 
+      <div className="navbar-left">
+        <div className="navbar-name">Mark Ebel</div>
+        <ThemeToggle />
+      </div>
+      <ul className="navbar-links">
         <li><NavLink to="./" end>Home!</NavLink></li>
         <li><NavLink to="AboutMe">About me</NavLink></li>
         <li><NavLink to="Blog">Blog</NavLink></li>

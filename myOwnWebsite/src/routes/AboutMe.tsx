0c16ae0480beae6438a18f12d83e36ec431075cd
Mark Ebel <markbenebel@gmail.com>
2024-10-02 21:53:03 +0100

    about section started and minor css changes

--- myOwnWebsite/src/routes/AboutMe.tsx
+++ myOwnWebsite/src/routes/AboutMe.tsx
@@ -1,10 +1,28 @@
-import WorkTimeline from '../components/Timeline';
+import astonLogo from '../assets/AstonLogo.png';
+import exeterLogo from '../assets/ExeterLogo.png';
+import redwoodTechLogo from '../assets/RedwoodTechLogo.png';
+//import '/src/css/AboutMe.css'
 
 const AboutMe = () => {
   return (
-    <div>This is a test hire me
-        
-      <WorkTimeline />
+    <div>
+      <div className="intro">
+
+        {/* <p>Hello, I'm Mark! I am a software developer, who enjoys writing code.</p> */}
+      </div>
+      <div className="AboutSection">
+        <img src={redwoodTechLogo} className="companyLogo" alt="Redwood Technologies Ltd" />
+        <p>I've started my career at Redwood Technologies (also known as Content Guru). Where I've learnt...</p>
+      </div>
+      <div className="AboutSection">
+        <img src={exeterLogo} className="companyLogo" alt="University of Exeter" />
+        <p>I attended the University of Exeter, I completed a masters in Data Science with Artificial Intelligence.</p>
+      </div>
+      <div className="AboutSection">
+        <img src={astonLogo} className="companyLogo" alt="Aston University" />
+        <p>At Aston University completed a Bsc in Computer Science.</p>
+      </div>
+
     </div>
   );
 };

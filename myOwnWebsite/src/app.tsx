fe19b29ee1759e07168039c7faa51b791c2fcd2e
Mark Ebel <markbenebel@gmail.com>
2025-02-11 19:03:38 +0000

    add torch effect

--- myOwnWebsite/src/app.tsx
+++ myOwnWebsite/src/app.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Section from "./components/section/section";
 import Sidebar from "./components/sidebar/sidebar";
 import Projects from "./features/sections/projects";
@@ -6,8 +7,31 @@ import About from "./features/sections/about";
 import "./app.css";
 
 const App = () => {
+  // State to track mouse position
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    // Update mouse position on move
+    const handleMouseMove = (e: MouseEvent) => {
+      setMousePos({ x: e.clientX, y: e.clientY });
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    
+    return () => document.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
   return (
     <div className="app">
+      {/* Torch Effect */}
+      <div
+        className="torch-effect"
+        style={{
+          background: `radial-gradient(circle 410px at ${mousePos.x}px ${mousePos.y}px, rgba(28, 15, 216, 0.14),rgba(17, 23, 42, 0.25))`
+        }}
+      ></div>
+
+      {/* Main Content */}
       <Sidebar />
       <main className="main-content">
         <Section id="about">

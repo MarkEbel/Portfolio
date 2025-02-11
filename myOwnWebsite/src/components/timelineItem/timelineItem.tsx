ba409acb391bccf3ff428c39b3f414ae6255b17e
Mark Ebel <markbenebel@gmail.com>
2025-02-11 22:16:26 +0000

    add skill bubbles

--- myOwnWebsite/src/components/timelineItem/timelineItem.tsx
+++ myOwnWebsite/src/components/timelineItem/timelineItem.tsx
@@ -1,25 +1,35 @@
 import "./timelineItem.css";
 
 interface TimelineItemProps {
-    id: string;
-    title: string;
-    company: string;
-    hyperlinkTitle: string;
-    date: string;
-    description: string;
-    skills: string;
-  }
-  
-  const TimelineItem = ({ id, date, description, title, company, hyperlinkTitle, skills }: TimelineItemProps) => {
-    return (
-      <section id={id} className="TimelineItem">
-          <p>{date}</p>
-          <h2>{title}</h2>
-          <h3><a href={hyperlinkTitle} target="_blank" rel="noopener noreferrer">{company}</a></h3>
-          <p>{description}</p>
-          <p>{skills}</p>
-      </section>
-    );
-  };
-  
-  export default TimelineItem;
+  id: string;
+  title: string;
+  company: string;
+  hyperlinkTitle: string;
+  date: string;
+  description: string;
+  skills: string[];
+}
+
+const TimelineItem = ({ id, date, description, title, company, hyperlinkTitle, skills }: TimelineItemProps) => {
+  return (
+    <section id={id} className="TimelineItem">
+      <p>{date}</p>
+      <h2>{title}</h2>
+      <h3>
+        <a href={hyperlinkTitle} target="_blank" rel="noopener noreferrer">
+          {company}
+        </a>
+      </h3>
+      <p>{description}</p>
+      <div className="skills-container">
+        {skills.map((skill, index) => (
+          <span key={index} className="skill-bubble">
+            {skill}
+          </span>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TimelineItem;
--- myOwnWebsite/src/features/sections/experience.tsx
+++ myOwnWebsite/src/features/sections/experience.tsx
@@ -11,20 +11,19 @@ const Experience = () => {
            company="Content Guru"
            hyperlinkTitle="https://www.contentguru.com/en-gb/about-us/"
            date="August 2022 - Present"
-           description=" I've started my career at Redwood Technologies (also known as Content
-            Guru). Where I've learnt..."
-           skills="list of skills"/>
+           description=" I started my career at Redwood Technologies, also known as Content Guru, where I have developed my expertise in software development. I worked extensively with C#, PHP, and JavaScript, contributing to scalable and robust applications while gaining experience in API development and cloud-based solutions."
+           skills={["SAST", "SCA", "FedRAMP", "PCI Compliance", "Kubernetes", "Docker", "ArgoCD", ".NET (C#)", "MSSQL", "PHP", "JavaScript", "React JS"]} />
         </SubSection>
         <SubSection id="exeter">
-          <TimelineItem
-           id="exeter"
-           title="MSc Data Science With AI"
-           company="University of Exeter"
-           hyperlinkTitle="https://www.exeter.ac.uk/"
-           date="2021 - 2022"
-           description="I attended the University of Exeter, I completed a masters in Data
-          Science with Artificial Intelligence."
-           skills="list of skills"/>
+        <TimelineItem
+          id="exeter"
+          title="MSc Data Science With AI"
+          company="University of Exeter"
+          hyperlinkTitle="https://www.exeter.ac.uk/"
+          date="2021 - 2022"
+          description="My studies focused on machine learning, big data analytics, and AI-driven decision-making, equipping me with the skills to analyze and interpret complex datasets. I conducted research on predicting Athletic Performance and trying to push AI into the sporting world."
+          skills={["AI", "Data Science", "Machine Learning", "Python", "Data Modelling"]}
+        />
         </SubSection>
         <SubSection id="aston">
           <TimelineItem
@@ -33,8 +32,8 @@ const Experience = () => {
            company="Aston University"
            hyperlinkTitle="https://www.aston.ac.uk/"
            date="2018 - 2021"
-           description="At Aston University completed a Bsc in Computer Science."
-           skills="list of skills"/>  
+           description="I built a strong foundation in programming, algorithms, and software engineering. During my time there, I gained experience with various technologies, including Java and Web Technologies, and developed a keen interest in problem-solving and system design."
+           skills={["Problem solving", "Software design", "Java"]} />
         </SubSection>
     </div>
   );
